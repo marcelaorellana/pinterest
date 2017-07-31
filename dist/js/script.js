24b4f67afb8bf48652180dc329ef22b2136e68d9
marcelaorellana <marcelapaz.1985@gmail.com>
2017-07-30 21:50:17 -0400

    navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -12641,15 +12641,16 @@ $(document).ready(function(){
 		var descripcion = $('<p>').attr('class', 'descrip');
 			descripcion.text(data[i].description);
 		var div = $('<div>').attr('class', 'divUsuario');
-		var usuario = $('<p>').text(data[i].username);
-		var hashtag = $('<p>').text('#' + data[i].hashtag);
+		var usuario = $('<p class="usuario">').append('<p><i class="fa fa-user-circle" aria-hidden="true"></i>'+data[i].username + '</p>');
 
-		var modalHeader = '<div><div><i class="fa fa-upload" aria-hidden="true"></i><i class="fa fa-check" aria-hidden="true"></i><i class="fa fa-ellipsis-h" aria-hidden="true"></i></div><button>Guardar</button></div>';
+		var hashtag = $('<p class="hashtag">').text('#' + data[i].hashtag);
+
+		var modalHeader = '<div class="modalHeader"><div><i class="fa fa-upload" aria-hidden="true"></i><i class="fa fa-check" aria-hidden="true"></i><i class="fa fa-ellipsis-h" aria-hidden="true"></i></div><button class="btn-guardar"><i class="fa fa-thumb-tack" aria-hidden="true"></i>Guardar</button></div>';
 		var modalImage = '<h5>'+ data[i].title + '</h5><img src="dist/img/' + data[i].image_url +'">';
 
-		var modalFooter = '<div><div><i class="fa fa-user-circle" aria-hidden="true"></i>'+ data[i].username +'</div><button>Leerlo</button></div><div>'+ data[i].description +'</div>';
+		var modalFooter = '<div class="usuario"><div class="usuario-central"><i class="fa fa-user-circle" aria-hidden="true"></i>'+ data[i].username +'</div><button class="btn-leer">Leerlo</button></div><div>'+ data[i].description +'</div>';
 
-		var modal = $('<div class="modal fade  bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><div class="modal-dialog modal-sm" role="document"><div class="modal-content"><div class="modal-body"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+ modalHeader + modalImage + modalHeader + modalFooter +'<button type="button" class="btn btn-default" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Close</button></div></div></div></div>');
+		var modal = $('<div class="modal fade  bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><div class="modal-dialog modal-sm" role="document"><div class="modal-content"><div class="modal-body"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+ modalHeader + modalImage + modalHeader + modalFooter +'</div></div></div></div>');
 
 
 
